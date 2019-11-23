dab3ca33b2779096568d58ff31649baeb42172c7
Collin Lyons <collinclyons@gmail.com>
2019-11-22 16:44:50 -0800

    Updates

---
+++ precache-manifest.d180375afd9592013052859bdb36c7eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e023a152bf1e974d9c88",
+    "url": "/static/css/main.76c119c7.chunk.css"
+  },
+  {
+    "revision": "e023a152bf1e974d9c88",
+    "url": "/static/js/main.e023a152.chunk.js"
+  },
+  {
+    "revision": "af84f7b489bb4767ce88",
+    "url": "/static/css/1.18a7e1d9.chunk.css"
+  },
+  {
+    "revision": "af84f7b489bb4767ce88",
+    "url": "/static/js/1.af84f7b4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f6aa71301ed46c66dd32f5ecb20cc1e0",
+    "url": "/index.html"
+  }
+];
